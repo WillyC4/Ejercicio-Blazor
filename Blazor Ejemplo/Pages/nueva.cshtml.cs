7258fae3904baec14f0f23eb0ee36925210ee8aa
wilyc <wilyc@SATELLITE>
2023-10-24 23:40:50 -0500

    Agregar archivos de proyecto.

---
+++ Blazor Ejemplo/Pages/nueva.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Blazor_Ejemplo.Pages
+{
+    public class nuevaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
